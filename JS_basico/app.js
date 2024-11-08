f7b740bedc3a6100f991e9be093a0c48e8caae5c
Nicolas <unipnicolas@gmail.com>
2024-11-08 14:22:15 -0300

    testando .log

--- JS_basico/app.js
+++ JS_basico/app.js
@@ -1,2 +1,17 @@
+alert('Boas vindas ao jg do Ne secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um Ne entre 1 e 10');
 
-alert()
+if (chute == numeroSecreto ) {
+    console.log('Vc acertou!!')
+} else {
+        console.log('VC errou o N° secreto!! E');   
+
+        if (chute > numeroSecreto) {
+            console.log('O N° secreto é menor')
+        } else {
+                console.log('O N° secreto é maior')
+            } 
+    }
+
+
